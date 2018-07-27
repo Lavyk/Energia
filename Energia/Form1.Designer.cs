e652075be31c2c1faf1232ac2266381c59ebb90b
lavyk_@hotmail.com <lavyk_@hotmail.com>
2018-07-27 07:34:22 -0300

    #

---
+++ Energia/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Energia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelKvhAtual = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.labelReaisAtual = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Name = "panel2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // panel4
+            // 
+            resources.ApplyResources(this.panel4, "panel4");
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel4.Controls.Add(this.labelReaisAtual);
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Name = "panel4";
+            // 
+            // panel3
+            // 
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.labelKvhAtual);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Name = "panel3";
+            // 
+            // labelKvhAtual
+            // 
+            resources.ApplyResources(this.labelKvhAtual, "labelKvhAtual");
+            this.labelKvhAtual.ForeColor = System.Drawing.Color.Lime;
+            this.labelKvhAtual.Name = "labelKvhAtual";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Name = "label4";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Name = "panel1";
+            // 
+            // panel5
+            // 
+            resources.ApplyResources(this.panel5, "panel5");
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Controls.Add(this.label6);
+            this.panel5.Name = "panel5";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Name = "label6";
+            // 
+            // panel6
+            // 
+            resources.ApplyResources(this.panel6, "panel6");
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel6.Name = "panel6";
+            // 
+            // labelReaisAtual
+            // 
+            resources.ApplyResources(this.labelReaisAtual, "labelReaisAtual");
+            this.labelReaisAtual.ForeColor = System.Drawing.Color.Lime;
+            this.labelReaisAtual.Name = "labelReaisAtual";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Name = "label8";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelKvhAtual;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelReaisAtual;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Energia/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace Energia
+{
+    public partial class Form1 : Form
+    {
+
+        DateTime DataHoraAbertura;
+        int seg, min, horas, dias;
+        double mins;
+        String strSeg, strMin, strHoras, strDias;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;  
+            timer1.Tick += new EventHandler(timer1_Tick); 
+            timer1.Enabled = true;
+            ConnectionOptions options = new ConnectionOptions();
+            ManagementScope scope = new ManagementScope();
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery(
+            "SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                // Display the remote computer information
+                Console.WriteLine("Computer Name : {0}",
+                    m["csname"]);
+                Console.WriteLine("Windows Directory : {0}",
+                    m["WindowsDirectory"]);
+                Console.WriteLine("Operating System: {0}",
+                    m["Caption"]);
+                Console.WriteLine("SerialNumber: {0}", m["SerialNumber"]);
+                Console.WriteLine("Manufacturer : {0}",
+                    m["Manufacturer"]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataHoraAbertura = DateTime.Now;
+            timer1.Start();
+        }
+
+        /* private void timer1_Tick(object sender, EventArgs e)
+        {
+            DiferencaDataHora = (DateTime.Now).Subtract(DataHoraAbertura);
+            this.label1.Text = DiferencaDataHora.ToString();
+            //Pode ser feito assim também
+
+            this.label1.Text = String.Format("Dias: {0}, Horas: {1}, Minutos: {2}, Segundos: {3}", DiferencaDataHora.Days, DiferencaDataHora.Hours, DiferencaDataHora.Minutes, DiferencaDataHora.Seconds);
+            this.label1.Text = this.DataHoraAbertura.ToString();
+        } */
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Contador de tempo
+
+            if (seg < 60)
+            {
+                seg++;
+            }
+            else
+            {
+                min++;
+                seg = 0;
+            }
+
+            if (min == 60)
+            {
+                horas++;
+                min = 0;
+            }
+
+            if (horas == 24)
+            {
+                dias++;
+                horas = 0;
+            }
+
+
+            if (seg <= 9)
+            {
+                this.strSeg = "0" + seg;
+            }
+            else
+            {
+                this.strSeg = seg.ToString();
+            }
+
+            if (min <= 9)
+            {
+                strMin = "0" + min;
+            }
+            else
+            {
+                strMin = min.ToString();
+            }
+
+            if (horas <= 9)
+            {
+                strHoras = "0" + horas;
+            }
+            else
+            {
+                strHoras = horas.ToString();
+            }
+
+            if (dias <= 9)
+            {
+                strDias = "0" + dias;
+            }
+            else
+            {
+                strDias = dias.ToString();
+            }
+
+            this.label1.Text = String.Format("{0}:{1}:{2}:{3}", this.strDias, this.strHoras, this.strMin, this.strSeg);
+            this.labelKvhAtual.Text = this.getKvHoras().ToString();
+            this.labelReaisAtual.Text = this.getCusto().ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double getKvHoras()
+        {
+            mins = (this.min / 6);
+            double tempo = this.horas + (mins/10);
+            double kvh = (260 * tempo);
+            return Math.Round(kvh / 1000, 2);
+        }
+
+        private double getCusto()
+        {
+            double preco = this.getKvHoras() * 0.49885;
+            return Math.Round(preco, 2); ;
+        }
+
+
+    }
+}
