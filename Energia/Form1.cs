df34b0f79185185e716b1d84cccaea3abf6199b4
lavyk_@hotmail.com <lavyk_@hotmail.com>
2018-07-28 10:27:37 -0300

    #

--- Energia/Form1.cs
+++ Energia/Form1.cs
@@ -30,7 +30,7 @@ namespace Energia
             scope.Connect();
 
             ObjectQuery query = new ObjectQuery(
-            "SELECT * FROM Win32_OperatingSystem");
+            "SELECT * FROM Win32_CurrentProbe");
             ManagementObjectSearcher searcher =
                 new ManagementObjectSearcher(scope, query);
 
@@ -38,15 +38,10 @@ namespace Energia
             foreach (ManagementObject m in queryCollection)
             {
                 // Display the remote computer information
-                Console.WriteLine("Computer Name : {0}",
-                    m["csname"]);
-                Console.WriteLine("Windows Directory : {0}",
-                    m["WindowsDirectory"]);
-                Console.WriteLine("Operating System: {0}",
-                    m["Caption"]);
-                Console.WriteLine("SerialNumber: {0}", m["SerialNumber"]);
-                Console.WriteLine("Manufacturer : {0}",
-                    m["Manufacturer"]);
+                Console.WriteLine("NormalMax : {0}",
+                    m["NormalMax"]);
+                Console.WriteLine("CurrentReading: {0}",
+                    m["CurrentReading"]);
             }
         }
 
